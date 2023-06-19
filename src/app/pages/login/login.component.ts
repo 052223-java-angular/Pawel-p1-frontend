65a0164ae338023a7c7ab3993b388a1784059376
Pawel Laciak <laciakp1@gmail.com>
2023-06-19 00:41:41 -0500

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
+import { ToastrService } from 'ngx-toastr';
+import { LoginPayload } from 'src/app/models/login-payload';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  validUsername: boolean = true;
+  
+  constructor(private fb: FormBuilder, 
+    private authService: AuthServiceService,  
+    private router: Router, 
+    private toastr:ToastrService ) {}
+
+    
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      
+  });
+  }
+
+  submitForm(): void {
+
+    if(this.loginForm && this.loginForm.invalid){
+      
+      if(!this.loginForm.controls['username'].value.test("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$")){
+        this.validUsername = false;
+      }
+      
+
+
+      this.loginForm.controls['username'].markAsTouched();
+      this.loginForm.controls['password'].markAsTouched();
+      this.loginForm.reset();
+      return;
+    }
+
+    //this.loginForm.reset();
+
+    const payload: LoginPayload = {
+      username: this.loginForm.controls['username'].value,
+      password: this.loginForm.controls['password'].value,
+      };
+    console.log("Username Entered: ", payload.username);
+      console.log("Password Entered: ", payload.password);
+    // //service sends payload to backend 
+
+    this.authService.login(payload).subscribe( {
+      next: value => {
+         this.toastr.success('Login Successful');
+         this.router.navigate(['/reviews']);
+         
+    },
+      
+      error: error => {console.log(error.error.message);
+        
+        this.toastr.error('Error');
+        
+
+  } 
+    
+    }); 
+  }
 
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,6 +3,7 @@ import{ FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { RegisterPayload } from 'src/app/models/register-payload';
 import { AuthServiceService } from 'src/app/services/auth-service.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,31 +13,32 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
 
   registerForm!: FormGroup;
-  constructor(private fb: FormBuilder, private authService: AuthServiceService,  private router: Router) {}
+  validUsername: boolean = true;
+  constructor(private fb: FormBuilder, private authService: AuthServiceService,  private router: Router,private toastr:ToastrService ) {}
 
     
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      username: ['', Validators.required],
+      username: ['', Validators.required, Validators.pattern("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$")],
       password: ['', Validators.required],
       confirmedPassword: ['', Validators.required],
-  })
+  });
   }
 
   submitForm(): void {
 
-      // Log form values
-  
-  console.log(this.registerForm.value);
-
-  // Log form errors
-  console.log(this.registerForm.errors);
     if(this.registerForm && this.registerForm.invalid){
-      console.log('This form is invalid');
-      //this.registerForm.reset();
+      
+
+      this.registerForm.controls['username'].markAsTouched();
+      this.registerForm.controls['password'].markAsTouched();
+      this.registerForm.controls['confirmedPassword'].markAsTouched();
+      this.registerForm.reset();
       return;
     }
+
+    //this.registerForm.reset();
 
     const payload: RegisterPayload = {
       username: this.registerForm.controls['username'].value,
@@ -50,14 +52,19 @@ export class RegisterComponent implements OnInit {
 
     this.authService.register(payload).subscribe( {
       next: value => {
+         this.toastr.success('Reigstration Successful');
          this.router.navigate(['/login']);
+         
     },
-      error: err => {console.log(err);},
       
+      error: error => {console.log(error.error.message);
+        
+        this.toastr.error('Error');
+        
+
+  } 
     
     }); 
-
-
   }
 
 }
