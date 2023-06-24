dd63f98f79459e4df11c8d4e1705cbc3662ce74b
Pawel Laciak <laciakp1@gmail.com>
2023-06-24 10:16:40 -0500

    homescreen wo video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  images = [
+    { url: 'https://halfacrebeer.com/wp-content/uploads/2021/08/MicrosoftTeams-image-177-1-1024x819.jpg', description: 'Half Acre Bodem' },
+    
+  ];
 }
